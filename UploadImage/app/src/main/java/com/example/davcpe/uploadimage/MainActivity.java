2ceec17187c02ce6aa2e8d8303080292687c5ae7
davit sanpote <supawadeewithdavit@gmail.com>
2015-03-27 00:44:36 +1000

    Update UploadImage2

--- UploadImage/app/src/main/java/com/example/davcpe/uploadimage/MainActivity.java
+++ UploadImage/app/src/main/java/com/example/davcpe/uploadimage/MainActivity.java
@@ -1,39 +1,103 @@
 package com.example.davcpe.uploadimage;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private ListView GetAllCustomerListView;
+    private JSONArray jsonArray;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.GetAllCustomerListView = (ListView) this.findViewById(R.id.GetAllCustomerListView);
+
+        new GetAllCustomerTask().execute(new ApiConnector());
+
+        this.GetAllCustomerListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                try
+                {
+                    // GEt the customer which was clicked
+                    JSONObject customerClicked = jsonArray.getJSONObject(position);
+
+                    // Send Customer ID
+                    Intent showDetails = new Intent(getApplicationContext(),CustomerDetailsActivity.class);
+                    showDetails.putExtra("CustomerID", customerClicked.getInt("id"));
+
+                    startActivity(showDetails);
+
+                }
+                catch (JSONException e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+
+    public  void setListAdapter(JSONArray jsonArray)
+    {
+        this.jsonArray = jsonArray;
+        this.GetAllCustomerListView.setAdapter(new GetAllCustomerListViewAdapter(jsonArray,this));
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+
+    private class GetAllCustomerTask extends AsyncTask<ApiConnector,Long,JSONArray>
+    {
+        @Override
+        protected JSONArray doInBackground(ApiConnector... params) {
+
+            // it is executed on Background thread
+
+            return params[0].GetAllCustomers();
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+
+            setListAdapter(jsonArray);
+
+
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        new GetAllCustomerTask().execute(new ApiConnector());
     }
 }
