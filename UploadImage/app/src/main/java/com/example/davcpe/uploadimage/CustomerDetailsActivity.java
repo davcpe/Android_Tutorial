10da6150b39b1111c31270babb68fa2a4a29686a
davit sanpote <supawadeewithdavit@gmail.com>
2015-03-27 00:43:25 +1000

    Update UploadIamge

---
+++ UploadImage/app/src/main/java/com/example/davcpe/uploadimage/CustomerDetailsActivity.java
@@ -0,0 +1,207 @@
+package com.example.davcpe.uploadimage;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.util.Base64;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by davcpe on 3/27/2015.
+ */
+public class CustomerDetailsActivity extends Activity {
+    private EditText firstName;
+    private EditText lastName;
+    private EditText age;
+
+    private int CustomerID;
+
+
+    //
+    private ImageView picture;
+    private static final String baseUrlForImage = "http://192.168.0.2/php/images/";
+    private Button changeImageButton;
+    private static final int SELECT_PICTURE = 1;
+    private String idOfCustomer;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_details);
+
+        this.firstName = (EditText) this.findViewById(R.id.firstName);
+        this.lastName = (EditText) this.findViewById(R.id.lastName);
+        this.age = (EditText) this.findViewById(R.id.age);
+
+        this.picture = (ImageView) this.findViewById(R.id.pic);
+
+
+        // get Customer ID
+        this.CustomerID = getIntent().getIntExtra("CustomerID", -1);
+
+        if (this.CustomerID > 0)
+        {
+            // we have customer ID passed correctly.
+            new GetCustomerDetails().execute(new ApiConnector());
+        }
+
+        this.changeImageButton = (Button) this.findViewById(R.id.changeImage);
+        this.changeImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,"Select Picture"), SELECT_PICTURE);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                if (Build.VERSION.SDK_INT < 19) {
+                    String selectedImagePath = getPath(selectedImageUri);
+                    Bitmap bitmap = BitmapFactory.decodeFile(selectedImagePath);
+                    SetImage(bitmap);
+                } else {
+                    ParcelFileDescriptor parcelFileDescriptor;
+                    try {
+                        parcelFileDescriptor = getContentResolver().openFileDescriptor(selectedImageUri, "r");
+                        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+                        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                        parcelFileDescriptor.close();
+                        SetImage(image);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private void SetImage(Bitmap image) {
+        this.picture.setImageBitmap(image);
+
+        // upload
+        String imageData = encodeTobase64(image);
+        final List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("image", imageData));
+        params.add(new BasicNameValuePair("CustomerID", idOfCustomer));
+
+        new AsyncTask<ApiConnector,Long, Boolean >() {
+            @Override
+            protected Boolean doInBackground(ApiConnector... apiConnectors) {
+                return apiConnectors[0].uploadImageToserver(params);
+            }
+        }.execute(new ApiConnector());
+
+    }
+
+    public static String encodeTobase64(Bitmap image) {
+        System.gc();
+
+        if (image == null)return null;
+
+        Bitmap immagex = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        immagex.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+
+        byte[] b = baos.toByteArray();
+
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT); // min minSdkVersion 8
+
+        return imageEncoded;
+    }
+
+
+    public String getPath(Uri uri) {
+        if( uri == null ) {
+            return null;
+        }
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        return uri.getPath();
+    }
+
+
+    private class GetCustomerDetails extends AsyncTask<ApiConnector,Long,JSONArray>
+    {
+        @Override
+        protected JSONArray doInBackground(ApiConnector... params) {
+
+            // it is executed on Background thread
+
+            return params[0].GetCustomerDetails(CustomerID);
+        }
+
+        @Override
+        protected void onPostExecute(JSONArray jsonArray) {
+
+            try
+            {
+                JSONObject customer = jsonArray.getJSONObject(0);
+
+                firstName.setText(customer.getString("FirstName"));
+                lastName.setText(customer.getString("LastName"));
+                age.setText(""+customer.getInt("Age"));
+
+                String urlForImage = baseUrlForImage + customer.getString("imageName");
+                new DownloadImageTask(picture).execute(urlForImage);
+                idOfCustomer = customer.getString("id");
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+}
